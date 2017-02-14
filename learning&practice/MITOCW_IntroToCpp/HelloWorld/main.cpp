76f17c2dcbb52f705712057efaf864ac009af5d4
Trung Dang <tdang229@uw.edu>
2017-02-14 12:51:04 -0800

    first project: helloworld. It worked finally!

---
+++ learning&practice/MITOCW_IntroToCpp/HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Trung on 2/14/2017.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    return 0;
+}
---
+++ learning&practice/MITOCW_IntroToCpp/Scope/sameName2inBlock/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Trung on 2/14/2017.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "HERE" << endl;
+    return 0;
+}
